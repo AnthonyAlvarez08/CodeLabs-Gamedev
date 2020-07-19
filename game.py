1b49af412e1426725ef60bba4c62f7dbf48a951b
wataflai <67923669+AnthonyAlvarez08@users.noreply.github.com>
2020-07-19 12:18:44 -0400

    no start on action card

--- game.py
+++ game.py
@@ -2,7 +2,7 @@
 Should we make our own custom cards?
 If need be I can just make the game be a function
 This is not finished yet
-TODO: uno yelling, drawing when can't play, add the rules, no start on action card
+TODO: uno yelling, drawing when can't play, add the rules
 """
 
 from unoClasses import new_card, Card, Player, previousCard
@@ -10,6 +10,10 @@ from unoClasses import new_card, Card, Player, previousCard
 numPlayers = int(input('How many players in this session?  '))
 
 players = [Player(f'Player {i + 1}') for i in range(numPlayers)]
+
+rules = input("Would you like to read the rules (y/n)?  ")
+if rules == 'y' or rules == 'yes':
+    print('''[insert rules]''')
 
 # prompts for a color, checks if it is acceptable and then returns it
 def color_change():
@@ -22,6 +26,22 @@ def color_change():
     
     return newColor
 
+# prompt to draw card and try to play or skip turn
+def unplayable(hand, topCard):
+    for i in hand:
+        if topCard.color == i.color:
+            return False
+        elif topCard.numAction == i.numAction:
+            return False
+        elif self.hand[index].color == 'change':
+            return False
+    return True
+    
+
+# no start with action card
+while previousCard.type == 'action':
+    previousCard = new_card()
+
 # f keeps track of whose turn it is and increment makes the turn changes
 f = 0
 increment = 1
@@ -30,11 +50,22 @@ while True:
     print(f'{players[f].name}\'s cards are:')
     for i, card in enumerate(players[f].hand):
         print(i, card)
+    
+    '''
+    if unplayable(players[f].hand, previousCard):
+        print('you have no cards that can be played!')
+        choice = input('would you like to skip your turn or draw a card? (skip/draw)')
+        if choice == 'skip' or choice == 's':
+            f += increment
+            continue
+        elif choice == 'draw' or choice == 'd':
+            players[f].draw_card()
+    '''            
 
     temp = previousCard
-    previousCard = players[f].play_card(temp)
+    previousCard = players[f].play_card(previousCard=temp)
     while previousCard == 'unacceptable':
-        previousCard = players[f].play_card(temp)
+        previousCard = players[f].play_card(previousCard=temp)
 
     if players[f].numCards == 0:
         print(f'{players[f].name} won!\n\nThank you so much for playing my game!')
@@ -42,6 +73,7 @@ while True:
 
     if previousCard.color == 'change':
         previousCard.color = color_change()
+        
     if previousCard.numAction == 'cancel':
         if f == numPlayers and increment == 1:
             f = 0
@@ -51,6 +83,7 @@ while True:
             f += 2 * increment
         print('\nNext player has been skipped!')
         continue
+
     if previousCard.numAction == 'reverse':
         # basically a toggle
         print('\nCycle Reversed')
@@ -81,5 +114,5 @@ while True:
     if f > numPlayers - 1:
          f = 0
     elif f < 0:
-         f += numPlayers - 1
+         f += numPlayers
     
