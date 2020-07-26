00ad40b4c012d18ff01a6db83cb666807cd117a4
Anna Chung <achung109@gmail.com>
2020-07-26 10:11:27 -0700

    added some js for text boxs and buttons

--- static/script.js
+++ static/script.js
@@ -1,3 +1,44 @@
-var socket = io.connect();
+/*var socket = io.connect();
 console.log("Hello world");
-socket.emit("join");
+socket.emit("join");*/
+
+
+var socket = io.connect('http://127.0.0.1:5000');
+
+    socket.on('connect', function() {
+		console.log("socket connected")
+	});
+
+	socket.on('join_room', function(msg){
+	    console.log("joined room")
+	});
+
+
+
+
+/*
+** how do we send message from server side? also not sure if this works
+
+    socket.on('receivedData', function (message) {
+        document.getElementById("text-box").innerText = message;
+    });
+
+    document.getElementById("send-btn").addEventListener('click', clickButton);
+
+    function clickButton() {
+        console.log("button clicked");
+        let inputBox = document.getElementById("input-box");
+        inputBox.value = "";
+        socket.emit('input, inputBox.value);
+    }
+*/
+
+	 socket.on('message', function(msg) {
+		$("#messages").append('<li>'+msg+'</li>');
+		console.log('Received message');
+	});
+
+	$('#sendbutton').on('click', function() {
+		socket.send($('#myMessage').val());
+		$('#myMessage').val('');
+	});
