6886fbd0a518bb4de0569e353f07389cce680756
Anna Chung <achung109@gmail.com>
2020-07-28 14:14:12 -0700

    moved some some code from here to html file

--- static/script.js
+++ static/script.js
@@ -31,14 +31,15 @@ var socket = io.connect('http://127.0.0.1:5000');
         inputBox.value = "";
         socket.emit('input, inputBox.value);
     }
-*/
 
+*/
 	 socket.on('message', function(msg) {
 		$("#messages").append('<li>'+msg+'</li>');
 		console.log('Received message');
 	});
-
+/*
 	$('#sendbutton').on('click', function() {
 		socket.send($('#myMessage').val());
 		$('#myMessage').val('');
 	});
+	*/
